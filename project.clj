f8d00e33626018503adf810b499d44165fa90f7e
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-07-30 20:11:00 +0600

    Let's start new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clokado "0.1.0"
+(defproject clokado "0.2.0-SNAPSHOT"
   :description "Simple Clojure library for Mikado Method"
   :url "https://github.com/ahitrin/clokado"
   :license {:name "Eclipse Public License"
