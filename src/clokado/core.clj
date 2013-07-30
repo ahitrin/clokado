fd5fe3082c00706f21fac9cf53bb19d12258702d
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-07-30 13:59:06 +0600

    Minor changes in terminology

--- src/clokado/core.clj
+++ src/clokado/core.clj
@@ -2,10 +2,10 @@
 
 ;; mikado rules:
 ;; 1. there's one goal at start, it's called 'mikado goal'
-;; 2. a goal is considered 'open' when there are no other open goals it depends on
-;; 4. a goal is considered 'top' when there are no goals it depends on
+;; 2. any new goal is considered 'open' by default
+;; 4. a goal is considered 'top' when there are no open goals it depends on
 ;; 5. we can choose any top goal to solve
-;; 6. when current goal cannot be solved, the new open goal is created. current goal now depends on it
+;; 6. when current goal cannot be solved, the new goal is created. current goal now depends on it
 ;; 7. when we achieve current goal, it becomes closed. now we can choose next goal to solve
 ;; 8. when we close mikado goal, the total problem is solved
 
