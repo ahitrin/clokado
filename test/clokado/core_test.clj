085c63c482387be17fe4fb4165832e19ce87fb33
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-07-30 13:00:07 +0600

    Refactoring: extract test function

--- test/clokado/core_test.clj
+++ test/clokado/core_test.clj
@@ -2,30 +2,32 @@
   (:use clojure.test
         clokado.core))
 
+(defn slice-should-be [goals field expected]
+  (is (= (map field goals) expected)))
+
 (deftest start-test
   (testing "there is one goal at start"
     (let [goals (mikado "Example goal")]
-      (is (= (count goals) 1))
-      (is (= (map :name goals) '("Example goal")))
-      (is (= (map :open goals) '(true)))
-      (is (= (map :depends goals) '([])))
-      (is (= (map :id goals) '(1))))))
+      (slice-should-be goals :name '("Example goal"))
+      (slice-should-be goals :open '(true))
+      (slice-should-be goals :depends '([]))
+      (slice-should-be goals :id '(1)))))
 
 (deftest add-one-node
   (testing "what happens when we add one goal to the mikado"
     (let [first-goal (mikado "Eat icecream")
           goals (add first-goal "Buy icecream")]
-      (is (= (map :name goals) '("Eat icecream" "Buy icecream")))
-      (is (= (map :open goals) '(true true)))
-      (is (= (map :depends goals) '([] [1])))
-      (is (= (map :id goals) '(1 2))))))
+      (slice-should-be goals :name '("Eat icecream" "Buy icecream"))
+      (slice-should-be goals :open '(true true))
+      (slice-should-be goals :depends '([] [1]))
+      (slice-should-be goals :id '(1 2)))))
 
 (deftest add-two-nodes
   (testing "what happens when we add more goals to the mikado"
     (let [first-goal (mikado "Kill the beast")
           next-goal (add first-goal "Prepare weapon")
           goals (add next-goal "Find the beast")]
-      (is (= (map :name goals) '("Kill the beast" "Prepare weapon" "Find the beast")))
-      (is (= (map :open goals) '(true true true)))
-      (is (= (map :depends goals) '([] [1] [1])))
-      (is (= (map :id goals) '(1 2 3))))))
+      (slice-should-be goals :name '("Kill the beast" "Prepare weapon" "Find the beast"))
+      (slice-should-be goals :open '(true true true))
+      (slice-should-be goals :depends '([] [1] [1]))
+      (slice-should-be goals :id '(1 2 3)))))
