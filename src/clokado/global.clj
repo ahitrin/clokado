e3f6e12f6e0e4f74fb135e9aad29c17886d99bef
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-08-01 00:12:24 +0600

    Add instant redraw for global library

--- src/clokado/global.clj
+++ src/clokado/global.clj
@@ -1,18 +1,26 @@
-(ns clokado.global (:use clokado.core))
+(ns clokado.global
+  (:use clokado.core
+        clokado.graphviz))
 
-"Simpler functions, seems more useful for REPL evaluation"
+"Simpler functions, seems more useful for REPL evaluation & instant redraw"
 
 (def tree (ref []))
+(def filename (ref ""))
 
-(defn mikado! [name]
-  (dosync (ref-set tree (mikado name))))
+(defn redraw! []
+  (to-png @tree @filename))
+
+(defn mikado! [name file]
+  (dosync (ref-set tree (mikado name))
+          (ref-set filename file))
+  (redraw!))
 
 (defn add!
-  ([name] (dosync (alter tree add name)))
-  ([name id] (dosync (alter tree add name id))))
+  ([name] (dosync (alter tree add name)) (redraw!))
+  ([name id] (dosync (alter tree add name id) (redraw!))))
 
 (defn top! []
   (top @tree))
 
 (defn close! [id]
-  (dosync (alter tree close id)))
+  (dosync (alter tree close id) (redraw!)))
