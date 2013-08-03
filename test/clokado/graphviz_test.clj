7e6534f4d6fa27723c650d973b8e502b6f75d7f1
Andrey Hitrin <andrey.hitrin@gmail.com>
2013-08-03 17:47:00 +0600

    Fix colors in tests

--- test/clokado/graphviz_test.clj
+++ test/clokado/graphviz_test.clj
@@ -15,16 +15,16 @@
   (testing "transformation of the smallest mikado tree"
     (is (= (to-graph simplest)
            '("digraph g {"
-             "1 [label=\"1: Just do it\", color=\"green\", shape=\"box\"];"
+             "1 [label=\"1: Just do it\", color=\"red\", shape=\"box\"];"
              "}")))))
 
 (deftest kitty-case
   (testing "transformation of more complex tree"
     (is (= (to-graph kitty)
            '("digraph g {"
-             "1 [label=\"1: Feed the kitty\", color=\"green\", shape=\"box\"];"
-             "2 [label=\"2: Find the food\", color=\"green\", shape=\"box\"];"
-             "3 [label=\"3: Go to the store\", color=\"green\", shape=\"box\"];"
+             "1 [label=\"1: Feed the kitty\", color=\"red\", shape=\"box\"];"
+             "2 [label=\"2: Find the food\", color=\"red\", shape=\"box\"];"
+             "3 [label=\"3: Go to the store\", color=\"red\", shape=\"box\"];"
              "2 -> 1;"
              "3 -> 2;"
              "}")))))
@@ -33,9 +33,9 @@
   (testing "closed goals should be colored red"
     (is (= (to-graph (close kitty 3))
            '("digraph g {"
-             "1 [label=\"1: Feed the kitty\", color=\"green\", shape=\"box\"];"
-             "2 [label=\"2: Find the food\", color=\"green\", shape=\"box\"];"
-             "3 [label=\"3: Go to the store\", color=\"red\", shape=\"box\"];"
+             "1 [label=\"1: Feed the kitty\", color=\"red\", shape=\"box\"];"
+             "2 [label=\"2: Find the food\", color=\"red\", shape=\"box\"];"
+             "3 [label=\"3: Go to the store\", color=\"green\", shape=\"box\"];"
              "2 -> 1;"
              "3 -> 2;"
              "}")))))
