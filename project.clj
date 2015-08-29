e6dbd9020e26cd6644f2b415aa11a5f65bc155c6
Andrey Hitrin <andrey.hitrin@gmail.com>
2015-08-30 01:19:00 +0500

    Add scm info to project file

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject clokado "0.3.6"
   :description "Simple Clojure library for Mikado Method"
   :url "https://github.com/ahitrin/clokado"
+  :scm {:name "git"
+        :url "https://github.com/ahitrin/clokado"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
