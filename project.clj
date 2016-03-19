8e2a82fc67e4936f38ff9c3e9ec2257052a8223d
Andrey Hitrin <andrey.hitrin@gmail.com>
2016-03-19 08:50:17 +0500

    Add test.check dependency

--- project.clj
+++ project.clj
@@ -5,5 +5,6 @@
         :url "https://github.com/ahitrin/clokado"}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [com.taoensso/nippy "2.9.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.taoensso/nippy "2.9.0"]]
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.9.0"]]}})
